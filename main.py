50eed8e7e425b5ac721f0dbcff5e30f3b8936e90
stelltriz <stellbea@icloud.com>
2024-03-27 11:18:02 -0300

    27/03

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ from models import Chute
 
 app = FastAPI()
 
-copos = [0,0,1]
+copos = [0, 0, 1]
 random.shuffle(copos)
-print(copos)
 
 if copos[0] == 1:
     resposta = 0
@@ -17,22 +16,25 @@ elif copos[1] == 1:
 elif copos[2] == 1:
     resposta = 2
 
-print(resposta)
 
-@app.get('/resposta')
+@app.get("/resposta")
 async def get_resposta():
     return resposta
 
-@app.post('/chute')
+
+@app.post("/chute")
 async def post_chute(chute: Chute):
     if chute.copo == resposta:
         print("Você Acertou!")
     elif chute.copo <= 3 and chute.copo >= 0 and chute.copo != resposta:
         print("Você errou!")
     else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Número Inválido")
-    
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Número Inválido"
+        )
 
-if __name__ == '__main__': #variável que o python cria sempre no arquivo principal
+
+if __name__ == "__main__":  # variável que o python cria sempre no arquivo principal
     import uvicorn
-    uvicorn.run("main:app", host='127.0.0.1', port=8000, reload=True)
+
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
